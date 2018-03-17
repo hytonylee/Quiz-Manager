8b249dd13fab9413d17922a5c362f3227bb00465
hytonylee <30307659+hytonylee@users.noreply.github.com>
2018-03-17 11:48:38 -0700

    complete homepage (#7)
    * complete homepage
    
    * change the setting icon

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from "react";
-import { Button, Checkbox, Form } from "semantic-ui-react";
-import logo from "./logo.svg";
-import "./App.css";
 
-class App extends Component {
+import React from 'react';
+import Homepage from './components/Homepage'
+
+
+
+
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+
+        <div className="App">
+          <Homepage />
+        </div>
+    )
+
   }
 }
 
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, Container, Divider, Icon } from 'semantic-ui-react';
+import 'bootstrap/dist/css/bootstrap.css';
+import '../index.css';
 
 
+class Homepage extends Component {
+  render () {
+    return (
+      <main className="Homepage mt-5">
 
+        <Container className="homepage-main-container">
+          <Container textAlign='center' className="homepage-container mt-5">
+            <h1>Welcome to CodeCore Drills</h1>
+          </Container>
+          <Container textAlign='center' className="homepage-container mt-3">
+            <Icon name='setting' size='huge' color='red' className='m-5' />
+            <Icon name='checkmark' size='huge' color='red' className='m-5' />
+            <Icon name='location arrow' size='huge' color='red' className='m-5' />
+          </Container>
+            <Divider />
+          <Container textAlign='center' className="homepage-container mt-3">
+            <Button color='black' size='huge' className="btn btn-primary btn-lg">Enter</Button>
+          </Container>
+        </Container>
 
-
+      </main>
+    )
+  }
+}
 
 export default Homepage
