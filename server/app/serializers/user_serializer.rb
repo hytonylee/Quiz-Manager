0f6c30a50ca849dd8db291ffb30ef194881fc219
Erika Balbino <35239603+erikabalbino@users.noreply.github.com>
2018-03-17 17:51:34 -0700

    Add User Serializer (#24)

---
+++ server/app/serializers/user_serializer.rb
@@ -0,0 +1,16 @@
+class UserSerializer < ActiveModel::Serializer
+
+  attributes :id, :first_name, :last_name, :email
+
+  has_many :quiz_takens
+
+  class QuizTakenSerializer < ActiveModel::Serializer
+    attributes :id, :score, :correct_answers
+
+    belongs_to :quiz
+    class QuizSerializer < ActiveModel::Serializer
+     attributes :id, :name, :description, :difficulty, :quiz_points
+     end
+  end
+
+end
